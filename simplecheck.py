d5d60c1e8f46244af04f4d4738d77195af62bffa
Wladimir J. van der Laan <laanwj@gmail.com>
2017-11-16 21:24:06 +0100

    add probing to simplecheck

--- simplecheck.py
+++ simplecheck.py
@@ -3,27 +3,35 @@
 Simple iteration+lookup check of databases.
 Should reproduce failures in case of corruption.
 '''
-# W.J. 2015
+# W.J. 2015-2017
 from __future__ import print_function,division
 import leveldb
 import sys
 
+N = 10000
+
+def checkdb(pathname):
+    print(' * opening %s' % pathname)
+    l2 = leveldb.LevelDB(pathname,paranoid_checks=True)
+    count = 0
+    for x in l2.RangeIter(verify_checksums=True):
+        x2 = l2.Get(x[0],verify_checksums=True)
+        assert(x2 == x[1])
+        count += 1
+    print(' * %i records checked' % count)
+    # "random" probe
+    print(' * probe %d times (trigger bloom filters)' % N)
+    for x in range(N):
+        key = x.to_bytes(8,byteorder="little")
+        try:
+            l2.Get(key, verify_checksums=True)
+        except KeyError:
+            pass
+
 base = sys.argv[1]
 
-print('Checking chain state')
-l = leveldb.LevelDB(base + '/chainstate',paranoid_checks=True)
-count = 0
-for x in l.RangeIter(verify_checksums=True):
-    x2 = l.Get(x[0],verify_checksums=True)
-    assert(x2 == x[1])
-    count += 1
-print('%i records checked' % count)
+print('* Checking chain state')
+checkdb(base + '/chainstate')
  
-print('Checking block index')
-l2 = leveldb.LevelDB(base + '/blocks/index',paranoid_checks=True)
-count = 0
-for x in l2.RangeIter(verify_checksums=True):
-    x2 = l2.Get(x[0],verify_checksums=True)
-    assert(x2 == x[1])
-    count += 1
-print('%i records checked' % count)
+print('* Checking block index')
+checkdb(base + '/blocks/index')
